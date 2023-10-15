ceee04eabbe310b81602b7b198077c72c8354542
neogugu <neogugu@yahoo.co.uk>
2023-10-15 15:35:22 +0100

    Initial stage

--- fantastic.py
+++ fantastic.py
@@ -3,3 +3,9 @@ cost_each = 2.99
 total_cost = (chickens * cost_each)
 print(total_cost)
 
+successful = True
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("successful")
+        break
